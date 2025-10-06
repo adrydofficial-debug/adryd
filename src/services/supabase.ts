28998258984c80c3b5761618a858335965f59bc0
ammar188 <ammarhit@gmail.com>
2025-10-06 17:40:50 +0500

    committing empty config.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '../config';
 
-export const supabase = createClient('url', 'key', {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
