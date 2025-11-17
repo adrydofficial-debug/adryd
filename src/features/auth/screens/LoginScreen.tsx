9d0c39078e59dbe72c79ac95dcea450cb887a1de
Your Name <your-email@example.com>
2025-11-17 15:49:59 +0500

    fixed

--- src/features/auth/screens/LoginScreen.tsx
+++ src/features/auth/screens/LoginScreen.tsx
@@ -4,7 +4,6 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Formik, FormikHelpers } from 'formik';
 import React, { useRef, useState } from 'react';
 import Loader from '../../../components/Loader';
-import Loader from '../../../components/Loader';
 import {
   ActivityIndicator,
   Dimensions,
